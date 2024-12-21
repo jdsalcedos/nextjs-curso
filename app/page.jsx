dbb98d3e90d6fcab50d9d9632c01cd6c5973cd23
Juan Salcedo <jdsalcedodv@gmail.com>
2024-12-20 19:30:12 -0500

    min 58:30

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,20 @@
+"use client"; // esto es si se quiere manejar el onClick en frontend, sino se quita el onClick ya que lo manejaría el b.e
+
+import Users from "../components/Users";
+
 export default function HomePage() {
-  return <h1>Hello world</h1>;
+  return (
+    <section>
+      <h1>Hello world</h1>
+      {/* no se podría poner el onClick sin el 'use client' */}
+      <button
+        onClick={() => {
+          alert("hello");
+        }}
+      >
+        Click
+      </button>
+      <Users />
+    </section>
+  );
 }
---
+++ components/Users.jsx
@@ -0,0 +1,14 @@
+//también se podría cambiar el 'use client' del HomePage y ponerlo aca. Eso en caso de no querer
+//que el HomePage sea un Client Component 
+import { useEffect } from "react";
+
+function Users() {
+  useEffect(() => {
+    alert("loaded"); //funciona porque esta siendo llamado desde HomePage y ahi esta el 'use client'
+  }, []);
+
+  return <div>Users</div>;
+}
+
+export default Users;
+
